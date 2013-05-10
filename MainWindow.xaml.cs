0584aa8b115a22d714c368fc073bd6f76aa790fa
ChengYouFang <s47548081@hotmail.com>
2013-05-11 02:43:25 +0800

    My first KINECT application

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Kinect;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor sensor;
+        private byte[] dataPixels;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region 視窗停止
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            sensor.Stop();
+        }
+
+        #endregion 視窗停止
+
+        #region 視窗載入
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            sensor = KinectSensor.KinectSensors[0];
+            sensor.ColorStream.Enable();
+            sensor.ColorFrameReady += VideoFrameReady;
+            sensor.Start();
+        }
+
+        #endregion 視窗載入
+
+        #region 持續讀入影像
+
+        private void VideoFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            bool data = false;
+            using (ColorImageFrame frame = e.OpenColorImageFrame())
+            {
+                if (frame == null)
+                {
+                }
+                else
+                {
+                    dataPixels = new byte[frame.PixelDataLength];
+                    frame.CopyPixelDataTo(dataPixels);
+                    data = true;
+                }
+            }
+            if (data)
+            {
+                BitmapSource source = BitmapSource.Create(640, 480, 96, 96, PixelFormats.Bgr32, null, dataPixels, 640 * 4);
+                videoImage.Source = source;
+            }
+        }
+
+        #endregion 持續讀入影像
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 MyFirstKinect
+ref:tw-hkt.blogspot.tw/2012/03/kinect-for-windows-sdk-v1.html
 =============
